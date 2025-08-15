b8435b6e19e780accaa2ea61aa6b3f7b60d88888
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-15 23:11:43 +0100

    telegram.py

--- backend/telegram.py
+++ backend/telegram.py
@@ -1,16 +1,11 @@
-import os
 import requests
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID_STOCKS = os.getenv("TELEGRAM_CHAT_ID_STOCKS")
-CHAT_ID_CRYPTO = os.getenv("TELEGRAM_CHAT_ID_CRYPTO")
-
-def send_telegram_message(text, is_crypto=False):
-    chat_id = CHAT_ID_CRYPTO if is_crypto else CHAT_ID_STOCKS
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+def send_telegram_message(bot_token, chat_id, message):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
-        "text": text,
-        "parse_mode": "Markdown"
+        "text": message,
+        "parse_mode": "HTML"
     }
-    requests.post(url, json=payload)
+    r = requests.post(url, data=payload)
+    return r.json()
