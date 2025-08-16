574001273e8b399c8f58955888bd12ba293147e1
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-16 17:17:24 +0100

    __init__.py

--- backend/providers/__init__.py
+++ backend/providers/__init__.py
@@ -1,1 +1,15 @@
+# Allow importing providers easily
+from .alphavantage import *
+from .coingecko import *
+from .coinglass import *
+from .coinmarketcal import *
+from .finnhub import *
+from .fmp import *
+from .lunarcrush import *
+from .messari import *
+from .newsapi import *
+from .polygon import *
+from .reddit import *
+from .santiment import *
+from .twitter import *
 
